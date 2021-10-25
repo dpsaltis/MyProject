46cc3bfccfd0fbae65dbe738b54350e6ebc18bf0
Dimitrios Psaltis <dpsaltis@email.arizona.edu>
2021-10-25 06:27:07 -0700

    my first attempt

---
+++ array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void func(double a[])
+{
+  a[0]=2.0;
+  return;
+
+}
+
+
+int main(void)
+{
+  double a[2]={1.0,1.0};
+
+  func(a);
+
+  printf("%f\n",a[0]);
+
+  return 0;
+}
+
+
+
