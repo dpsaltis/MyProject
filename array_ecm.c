667c888caeaa4c20eed3e36a0e5b024eddb507b9
Evan Mayer <evanmayer@uchicago.edu>
2021-11-01 10:38:57 -0700

    ecm change

---
+++ array_ecm.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+// this is dev
+
+void func(double a[])
+{
+  a[0]=2.0;
+  return;
+
+}
+
+
+int main(void)
+{
+  double a[2]={1.0,1.0};
+
+  func(a);
+
+  printf("%f\n",a[0]);
+
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+:)
